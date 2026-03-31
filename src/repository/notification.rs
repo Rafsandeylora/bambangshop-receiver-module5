dacf3e21a5388fecc668d7ae8dee4469a3bf9866
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2026-03-31 14:13:11 +0700

    Create Notification database and Notification repository struct skeleton.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -1,31 +1,15 @@
-use std::fmt::{Display, Formatter, Result};
+use std::sync::RwLock;
 
-use rocket::serde::{Deserialize, Serialize};
+use lazy_static::lazy_static;
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
-#[serde(crate = "rocket::serde")]
-pub struct Notification {
-    pub product_title: String,
-    pub product_url: String,
-    pub product_type: String,
-    pub subscriber_name: String,
-    pub status: String
+use crate::model::notification::Notification;
+
+// Singleton of Database
+lazy_static! {
+    static ref NOTIFICATIONS: RwLock<Vec<Notification>> = RwLock::new(vec![]);
 }
 
-impl Display for Notification {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        if self.status.to_uppercase().eq("CREATED") {
-            return write!(f,
-                "Hello {}, let's try our new {} product: {}, only in BambangShop! Check it out: {}",
-                self.subscriber_name, self.product_type.to_lowercase(), self.product_title, self.product_url);
-        } else if self.status.to_uppercase().eq("DELETED") {
-            return write!(f,
-                "Hello {}, we informed that our {} product called {} already sold out...",
-                self.subscriber_name, self.product_type.to_lowercase(), self.product_title);
-        } else {
-            return write!(f,
-                "Hello {}, let's try our {} product: {}, grab it out before the stock ran out! Check it out: {}",
-                self.subscriber_name, self.product_type.to_lowercase(), self.product_title, self.product_url);
-        }
-    }
+pub struct NotificationRepository;
+
+impl NotificationRepository {
 }
